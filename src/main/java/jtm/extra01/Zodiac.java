2cb430b285b6915b80f9fc4b16b7d5fcf0c53a46
IrinaTkacenko <irinatkacenko@inbox.lv>
2020-02-08 21:28:23 +0200

    Completed Zodiac task

--- src/main/java/jtm/extra01/Zodiac.java
+++ src/main/java/jtm/extra01/Zodiac.java
@@ -16,13 +16,53 @@ public class Zodiac {
 		// Look at wikipedia:
 		// https://en.wikipedia.org/wiki/Zodiac#Table_of_dates
 		// Tropical zodiac, to get appropriate date ranges for signs
+
+		boolean res;
+		switch (month) {
+			case 1:
+				zodiac = (day <= 20) ? "Capricorn" : "Aquarius";
+			break;
+			case 2:
+				zodiac = (day <= 19) ? "Aquarius" : "Pisces";
+				break;
+			case 3:
+				zodiac = (day <= 20) ? "Pisces" : "Aries";
+				break;
+			case 4:
+				zodiac = (day <= 20) ? "Aries" : "Taurus";
+				break;
+			case 5:
+				zodiac = (day <= 21) ? "Taurus" : "Gemini";
+				break;
+			case 6:
+				zodiac = (day <= 21) ? "Gemini" : "Cancer";
+				break;
+			case 7:
+				zodiac = (day <= 22) ? "Cancer" : "Leo";
+				break;
+			case 8:
+				zodiac = (day <= 22) ? "Leo" : "Virgo";
+				break;
+			case 9:
+				zodiac = (day <= 23) ? "Virgo" : "Libra";
+				break;
+			case 10:
+				zodiac = (day <= 23) ? "Libra" : "Scorpio";
+				break;
+			case 11:
+				zodiac = (day <= 22) ? "Scorpio" : "Sagittarius";
+				break;
+			case 12:
+				zodiac = (day <= 21) ? "Sagittarius" : "Capricorn";
+				break;
+		}
 		return zodiac;
 	}
 
 	public static void main(String[] args) {
 		// HINT: you can use main method to test your getZodiac method with
 		// different parameters
-		System.out.println(getZodiac(1, 1));
+		System.out.println(getZodiac(20, 2));
 	}
 
 }
