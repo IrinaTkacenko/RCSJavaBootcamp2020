0c3a4c648b11372fe74f1c5b81ce9bb6e2f3c490
IrinaTkacenko <irinatkacenko@inbox.lv>
2020-02-18 12:49:02 +0200

    Completed extra03

--- src/main/java/jtm/extra03/PracticalNumbers.java
+++ src/main/java/jtm/extra03/PracticalNumbers.java
@@ -1,6 +1,7 @@
 package jtm.extra03;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+
 
 public class PracticalNumbers {
 
@@ -8,7 +9,40 @@ public class PracticalNumbers {
 	// Implement method, which returns practical numbers in given range
 	// including
 	public String getPracticalNumbers(int from, int to) {
-		return "";
+		ArrayList<Integer> practicalNumbers = new ArrayList<>();
+		for (int i = from; i <= to; i++) {
+			if(checkIfPractical(i)) {
+				practicalNumbers.add(i);
+			}
+		}
+		return practicalNumbers.toString();
 	}
 
+	public boolean checkIfPractical(int number) {
+		ArrayList<Integer> divisors = new ArrayList<>();
+
+		for(int j = 1; j < number; j++) {
+			if(number % j == 0) {
+				divisors.add(j);
+			}
+		}
+		for(int i = number - 1; i > 0; i--) {
+			int currentNumber = i;
+			for (int j = divisors.size() - 1; j >= 0; j--) {
+				if(currentNumber > 0) {
+					currentNumber -= divisors.get(j);
+				}
+				if(currentNumber < 0) {
+					currentNumber += divisors.get(j);
+				}
+				if(currentNumber == 0) {
+					break;
+				}
+			}
+			if(currentNumber != 0) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
