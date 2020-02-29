cc7fc753d6c2c8a6d12663c77652bad4f22b3fb3
IrinaTkacenko <irinatkacenko@inbox.lv>
2020-02-29 20:30:54 +0200

    Completed extra4

--- src/main/java/jtm/extra04/Car.java
+++ src/main/java/jtm/extra04/Car.java
@@ -1,6 +1,9 @@
 package jtm.extra04;
 
-public class Car {
+import org.json.JSONString;
+import java.util.Locale;
+
+public class Car implements JSONString{
 
 	private String model;
 	private Integer year;
@@ -10,5 +13,35 @@ public class Car {
 	// TODO #1 generate public constructor which takes all properties of an
 	// object as parameters
 
+	public Car(String model, Integer year, String color, Float price) {
+		this.model = model;
+		this.year = year;
+		this.color = color;
+		this.price = price;
+	}
+
 	// TODO #2 generate public getters of all object properties
+	public String getModel() {
+		return model;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public Float getPrice() {
+		return price;
+	}
+
+	@Override
+	public String toJSONString() {
+		return "{\"model\":" + "\"" + model + "\"" + "," +
+				"\"year\":" + year + "," +
+				"\"color\":" + "\"" + color + "\"" + "," +
+				"\"price\":" + String.format(Locale.US, "%.0f", price) + "}";
+	}
 }
--- src/main/java/jtm/extra04/JsonCars.java
+++ src/main/java/jtm/extra04/JsonCars.java
@@ -1,8 +1,9 @@
 package jtm.extra04;
 
-import java.io.StringWriter;
-import java.util.ArrayList;
-import java.util.List;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.*;
 
 
 public class JsonCars {
@@ -10,14 +11,43 @@ public class JsonCars {
 	/*- TODO #1
 	 * Implement method, which returns list of cars from generated JSON string
 	 */
-	public List<Car> getCars(String jsonString) {
+	public List<Car> getCars(String jsonString){
 		/*- HINTS:
 		 * You will need to use:
 		 * - https://stleary.github.io/JSON-java/org/json/JSONObject.html
 		 * - https://stleary.github.io/JSON-java/org/json/JSONArray.html
 		 */
 
-		return null;
+		List<Car> someCars = new ArrayList<>();
+		JSONObject jsonObject = new JSONObject(jsonString);
+		String[] array = JSONObject.getNames(jsonObject);
+		for (String field:array) {
+			JSONArray jsonArray = new JSONArray(jsonObject.get(field).toString());
+			for(int i = 0; i < jsonArray.length(); i++) {
+				JSONObject jsonCar = new JSONObject(jsonArray.get(i).toString());
+				String[] carFields = JSONObject.getNames(jsonCar);
+				String[] orderedCarFields = new String[carFields.length];
+				for(String carField:carFields) {
+					switch (carField){
+						case "model":
+							orderedCarFields[0] = carField;
+							break;
+						case "year":
+							orderedCarFields[1] = carField;
+							break;
+						case "color":
+							orderedCarFields[2] = carField;
+							break;
+						case "price":
+							orderedCarFields[3] = carField;
+							break;
+					}
+				}
+				someCars.add(new Car(jsonCar.getString(orderedCarFields[0]),jsonCar.getInt(orderedCarFields[1]),
+						jsonCar.getString(orderedCarFields[2]),jsonCar.getFloat(orderedCarFields[3])));
+			}
+		}
+		return someCars;
 	}
 
 	/*- TODO #2
@@ -29,7 +59,11 @@ public class JsonCars {
 		 * - https://docs.oracle.com/javase/8/docs/api/index.html?java/io/StringWriter.html
 		 * - http://static.javadoc.io/org.json/json/20180130/index.html?org/json/JSONWriter.html
 		 */
-		return null;
+		JSONObject object = new JSONObject();
+		JSONArray array = new JSONArray();
+		for(Car car:cars) {
+			array.put(new Car(car.getModel(),car.getYear(),car.getColor(),car.getPrice()));
+		}
+		return object.put("cars", array).toString();
 	}
-
 }
